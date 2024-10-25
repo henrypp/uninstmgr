88da09503cbc5e0e046dc00f893f23c73e1df3da
henrypp <sforce5@mail.ru>
2024-10-26 01:19:23 +0500

    add app.h

---
+++ src/app.h
@@ -0,0 +1,11 @@
+// Uninstall Manager
+// Copyright (c) 2010-2025 Henry++
+
+#pragma once
+
+#define APP_NAME L"Uninstall Manager"
+#define APP_NAME_SHORT L"uninstmgr"
+#define APP_VERSION L"2.0"
+#define APP_VERSION_RES 2,0,0,0
+#define APP_AUTHOR L"Henry++"
+#define APP_COPYRIGHT L"(c) 2010-2025 " APP_AUTHOR L". All Rights Reversed."
