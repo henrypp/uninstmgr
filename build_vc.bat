9627b1ec399365a4939154a3ae47dbc5ae65638a
henrypp <sforce5@mail.ru>
2026-04-13 14:46:52 +0500

    upd compiler

--- build_vc.bat
+++ build_vc.bat
@@ -4,36 +4,21 @@ rem @cd /d "%~dp0\..\"
 
 rem VS 2026
 
-if exist "%ProgramFiles%\Microsoft Visual Studio\2026\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-	call "%ProgramFiles%\Microsoft Visual Studio\2026\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+if exist "%ProgramFiles%\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+	call "%ProgramFiles%\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 	goto start
 )
 
-if exist "%ProgramFiles%\Microsoft Visual Studio\2026\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
-	call "%ProgramFiles%\Microsoft Visual Studio\2026\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+if exist "%ProgramFiles%\Microsoft Visual Studio\18\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+	call "%ProgramFiles%\Microsoft Visual Studio\18\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 	goto start
 )
 
-rem VS 2022
-
-if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-	call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
-	goto start
-)
-
-if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
-	call "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
-	goto start
-)
-
-echo VS was not found...
+echo VS 2026 was not found...
 
 goto end
 
 :start
-
-msbuild uninstmgr.sln -property:Configuration=Release -property:Platform=x86 -verbosity:normal
-if %ERRORLEVEL% neq 0 goto end
 
 msbuild uninstmgr.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
